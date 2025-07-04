2b0fdde563b5b03192474c768b67737706e51fc0
MrvTo <krmplt2008@gmail.com>
2025-07-04 22:28:20 +0300

    Oyun detaylarındaki güncellemeler

--- src/pages/OyunDetay.js
+++ src/pages/OyunDetay.js
@@ -11,7 +11,7 @@ const oyunlar = [
       "Los Santos şehrinde geçen devasa açık dünya aksiyon oyunu.",
     minimum: "Intel Core 2 Quad CPU Q6600 / 4 GB RAM / NVIDIA 9800 GT 1GB",
     onerilen: "Intel Core i5 3470 / 8 GB RAM / NVIDIA GTX 660 2GB",
-    indirmeLink: "https://www.swiftuploads.com/download/eyJpdiI6IittRm1wNGJaVWNvbHFWQ3ZpYVJoeVE9PSIsInZhbHVlIjoiMlJHSVRoS0pVM0tyZ0VBWG9CZ0RsM3JQRFpVNmplOW1FM2F3R1dTWmZ6ST0iLCJtYWMiOiIwN2VmMzQ1ZGE0M2VjMjZlOTgyN2RmOGMyZDY2YWY5OWQ1OTAxNjM4YzQ3NjQwNTAzOTA2MTY2MzJlMjI4ZTE5IiwidGFnIjoiIn0=/GV3441.torrent?expiration=1751651072&signature=87ae0432532c2293d2f6a168747b48651f7cebd38faeded249586e601545bd90",
+    indirmeLink: "https://www.mediafire.com/file/m1sk63mn62zyb74/gtav_F%25C4%25B0tgrilReapck.torrent/file",
   },
   {
     id: "rdr2",
@@ -22,7 +22,7 @@ const oyunlar = [
       "Vahşi Batı’da epik açık dünya macerası.",
     minimum: "Intel Core i5-2500K / 8 GB RAM / Nvidia GTX 770",
     onerilen: "Intel Core i7-4770K / 12 GB RAM / Nvidia GTX 1060",
-    indirmeLink: "https://www.swiftuploads.com/download/eyJpdiI6ImV5cDRrNldBNGdtSnBCZ2JpOWdZUWc9PSIsInZhbHVlIjoiQjhhSXZNNFc1UFB1bGtnSU9DUGdOSlhLK2VoQWNqQ2hOVzhETUdZcGxuTT0iLCJtYWMiOiI2ZWJlNmI0MTlhZWYxZjA3NzA5YzQyMTU4ZGM3Zjk2YjI1MjZhZWIwNjY3N2YwNTgwZGZmYTQ5NDA3NjQ4OGU1IiwidGFnIjoiIn0=/DODI_REPACK1491.50%20%281%29.torrent?expiration=1751651398&signature=224f91454889b3fa6ce545a3a36929c821acb0d572cbe4ac228bc0085f05533d",
+    indirmeLink: "https://www.mediafire.com/file/l4wmqgtr0522p0y/rdr2_dodi_repack.torrent/file",
   },
   {
     id: "hogwartslegacy",
@@ -33,7 +33,7 @@ const oyunlar = [
       "Hogwarts’ta büyücülük ve macera dolu bir RPG oyunu.",
     minimum: "Intel Core i5-6600 / 16 GB RAM / Nvidia GTX 960",
     onerilen: "Intel Core i7-8700 / 16 GB RAM / Nvidia RTX 2060",
-    indirmeLink: "https://www.swiftuploads.com/download/eyJpdiI6IlUreXlyMTVvVUZkWFZTQ0dxVEo1MXc9PSIsInZhbHVlIjoidkh0bWZLR0RiMUJyQ1JYMkNrcWM0SUpaeS9xME1jclZKUmIzaDRheW9RTT0iLCJtYWMiOiIyMDU2MDRkMDI3NTA4Y2Q0MzZiYzM4MWMxZDljZjJjOWI2YjllNTdjNjI1ZGJmMmQ0ZWFhYzFiZTcyOTA0OWQ5IiwidGFnIjoiIn0=/Rip-Insan.torrent?expiration=1751651497&signature=f28b004b759a176e4f63e669512a9c5471265dc333400e9732ead43b4954fcc3",
+    indirmeLink: "https://www.mediafire.com/file/1yf8kjevoq04v4j/hogwarts_legeacy_dodi_repack.torrent/file",
   },
   {
     id: "uncharted4",
@@ -44,7 +44,7 @@ const oyunlar = [
       "Nate’in son macerası, sürükleyici aksiyon dolu oyun.",
     minimum: "Intel Core i5-2500K / 8 GB RAM / Nvidia GTX 660",
     onerilen: "Intel Core i7-4770K / 12 GB RAM / Nvidia GTX 1060",
-    indirmeLink: "http://itorrents.org/torrent/963A0879AF19DEB41A155EA5B9C0627397022D9B.torrent",
+    indirmeLink: "https://www.mediafire.com/file/h1li91172z4gle1/Uncharted_LoTC_FitgirlRepack.torrent/file",
   },
   {
     id: "cyberpunk2077",
@@ -55,7 +55,7 @@ const oyunlar = [
       "Night City’de futuristik aksiyon RPG.",
     minimum: "Intel Core i5-3570K / 8 GB RAM / Nvidia GTX 780",
     onerilen: "Intel Core i7-4790 / 12 GB RAM / Nvidia GTX 1060",
-    indirmeLink: "http://itorrents.org/torrent/179B76CFE2760F6B25B280F7D7A9040324460D6C.torrent",
+    indirmeLink: "https://www.mediafire.com/file/uphkb9rxxtyqnoo/Cyberpunk2077_FitgirlRepack.torrent/file",
   },
 ];
 
