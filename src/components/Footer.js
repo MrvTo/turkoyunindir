8be9e5560b95b886f0851e65f9967032a4e67dfa
MrvTo <krmplt2008@gmail.com>
2025-07-06 20:27:42 +0300

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../App.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>&copy; 2025 turktorrentindir — Tüm Hakları Saklıdır.</p>
+        <p>
+          Site deneme amaçlı yapılmıştır. Tüm linkler{" "}
+          <strong>dodi</strong>, <strong>fitgirl</strong>, <strong>chovka</strong>,{" "}
+          <strong>igruha</strong> gibi kaynaklardan alınmıştır. Virustotal üzerinden
+          kontrol edilebilir.
+        </p>
+        <p>Designed by <strong>MrvTo</strong> 🎮</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
