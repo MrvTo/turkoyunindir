3deacfc9c70191d3c8200355f9e5f6626949eb95
Kerem Polater <krmplt2008@gmail.com>
2025-10-16 23:38:47 +0300

    easteregg

--- src/pages/Iletisim.js
+++ src/pages/Iletisim.js
@@ -4,7 +4,7 @@ export default function Iletisim() {
   return (
     <div className="container mt-4">
       <h1>İletişim</h1>
-      <p>Bizimle iletişime geçmek için: <a href="daha hazır değil">hiçhazırdeğil</a></p>
+      <p>Ahmet Kaya derki: <a href="daha hazır değil">Bana diyor rica ederim dedi öyle bir ağırıma gitti ya dedim yaw bende sana rica ederim ben o zamanlar rica ederimin Anlamını bilmiyordum yani onu bir küfür gibi zannettim biliyor musun</a></p>
     </div>
   );
 }
