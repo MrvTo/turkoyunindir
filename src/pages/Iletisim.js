209c3e139dcb204deec47612f67943b08764d801
Kerem Polater <krmplt2008@gmail.com>
2025-10-16 23:45:56 +0300

    link eklendş

--- src/pages/Iletisim.js
+++ src/pages/Iletisim.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function Iletisim() {
   return (
     <div className="container mt-4">
-      <h1>İletişim</h1>
-      <p>Ahmet Kaya derki: <a href="daha hazır değil">Bana diyor rica ederim dedi öyle bir ağırıma gitti ya dedim yaw bende sana rica ederim ben o zamanlar rica ederimin Anlamını bilmiyordum yani onu bir küfür gibi zannettim biliyor musun</a></p>
+      <h1>Easter Egg</h1>
+      <p>Ahmet Kaya derki: <a href="https://www.youtube.com/watch?v=N__q5w3gz_E">Bana diyor rica ederim dedi öyle bir ağırıma gitti ya dedim yaw bende sana rica ederim ben o zamanlar rica ederimin Anlamını bilmiyordum yani onu bir küfür gibi zannettim biliyor musun</a></p>
     </div>
   );
 }
